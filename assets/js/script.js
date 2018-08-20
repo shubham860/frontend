044c015b16b548fd5bdfd6fcc9180911af4e2c56
@shubham860 <shubham.chauhan860@gmail.com>
2018-08-20 20:05:38 +0530

    update-7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,10 @@
-/*SIDE NAVIGATION*/
-var btnMenu = document.getElementById("btn-menu");
-btnMenu.addEventListener("click", toggleMenu);
-
-
-function toggleMenu(e) {
-
-    var divOverlay = document.getElementsByClassName("overlay")[0];
-    var speed = 10;
-
-
-    var eventTarget = e.target;
-
-    if( eventTarget.className == "btn-open" ) {
-        fadeIn(divOverlay, speed);
-        eventTarget.className = "btn-close";
-
-    } else if ( eventTarget.className == "btn-close") {
-        fadeOut(divOverlay, speed);
-        eventTarget.className = "btn-open";
-    }
-
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
-function fadeIn(elem, speed) {
-
-    var inInterval = setInterval(function(){
-
-    elem.style.opacity = Number(elem.style.opacity) + 0.02;
-
-    if (elem.style.opacity >= 1) {
-
-        elem.style.opacity = 1;
-        clearInterval(inInterval);
-
-    }
-
-
-    }, speed); // 10ms == .01s
-
-
-
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
-
-function fadeOut(elem, speed) {
-
-    var outInterval = setInterval(function(){
-
-    elem.style.opacity = Number(elem.style.opacity) - 0.02;
-
-    if (elem.style.opacity <= 0) {
-
-        elem.style.opacity = 0;
-        clearInterval(outInterval);
-
-    }
-
-
-    }, speed); // 10ms == .01s
-
-
-
-}
-/*animations*/
-AOS.init();
-
-
-$(document).ready(function(){
-    $("#btn-menu").click(function(){
-        $("#navlogo").slideUp(5000);
-        $("#menu").slideDown(5000);
-    });
-});
 
 
 
